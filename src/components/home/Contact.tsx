3ce874d3a063e74beb0dc2187741c57f539fa410
Smile <crowszero22@gmail.com>
2022-07-20 18:47:54 +0700

    feat: add github link

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -3,6 +3,7 @@ import EmailIcon from '@mui/icons-material/Email';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Contact = () => {
   return (
@@ -29,6 +30,13 @@ const Contact = () => {
           rel='noreferrer'
         >
           <LinkedInIcon />
+        </a>
+        <a
+          href='https://github.com/ssamyne/warma-project.git'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <GitHubIcon />
         </a>
       </div>
       <div className={classes.row}>
